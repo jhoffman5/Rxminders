20ad42a3dd6cb035e3ef5aa3c89721c7d165be95
Hoffman-Joshua <jhoffman5@carthage.edu>
2019-10-24 13:57:14 -0500

    Display Prescription data on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,25 +9,35 @@ import { Storage } from '@ionic/storage';
 })
 export class HomePage {
   
-  public prescriptions: string[] = this.allPrescriptions();
+  public prescriptions: any[] = this.allPrescriptions();
 
   constructor(private storage: Storage) {
-    this.prescriptions = this.allPrescriptions();
+    //this.prescriptions = this.allPrescriptions();
     //console.log(this.prescriptions);
   }
 
   allPrescriptions(){
     let retVal = [];
+    /*
     this.storage.keys()
     .then(
       data=>{
         retVal = data;
+        
         this.prescriptions = data;
         //console.log(data);
         //return data;
       },
       error => console.log(error)
-    );
+    );*/
+    this.storage.forEach((value:any, key:string, iterationNumber: Number)=>
+    {
+      console.log(key);
+      console.log(value);
+      //console.log(iterationNumber);
+
+      this.prescriptions.push(value);
+    });
     
     return retVal;
   }
