d22280384af7df71148988a09ad236a28a111b8a
Hoffman-Joshua <jhoffman5@carthage.edu>
2020-02-13 21:12:16 -0600

    Info page gets params

--- src/app/info-page/info-page.page.ts
+++ src/app/info-page/info-page.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Platform } from '@ionic/angular';
+import { AlertController } from  '@ionic/angular';
+
+import { Storage } from '@ionic/storage';
+import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
+import { NavController } from '@ionic/angular';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-info-page',
   templateUrl: './info-page.page.html',
@@ -7,9 +15,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InfoPagePage implements OnInit {
 
-  constructor() { }
+  public prescription;
+  public param;
 
-  ngOnInit() {
+  constructor(public navCtrl: NavController, private route: ActivatedRoute, private storage: Storage, private localNotifications: LocalNotifications, private plt: Platform, private alertCtrl: AlertController) {
+    this.param = this.route.snapshot.paramMap.get('preName').toString();
+    console.log(this.param);
+    this.getPrescription(this.param);
+    //.then((data)=>{this.prescription=this.getPrescription(this.param)});
   }
 
+  ngOnInit() {
+
+  }
+
+  ionViewWillEnter() {
+    
+  }
+
+  getPrescription(preName){
+    this.storage.get(preName);
+    console.log(this.prescription);
+  }
 }
